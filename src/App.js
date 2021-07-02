63a3fd899b369e2a88fadf0737de26a17b5143fe
Jaro Unger <jaro.unger@t-online.de>
2021-07-02 17:37:31 +0200

    Neu

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import './App.css';
 import Typography from '@material-ui/core/Typography';
 import Header from './components/Header';
 import Grid from './components/Grid';
-import Card from './components/Card';
 import { Component } from 'react';
+
 
 export default class App extends Component {
   render() {
